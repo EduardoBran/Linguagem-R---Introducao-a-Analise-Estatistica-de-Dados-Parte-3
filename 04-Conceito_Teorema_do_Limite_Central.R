c39a53281324300e91bcaf2511f4f2056b192368
Eduardo Brandao <eduardo.ads1814@gmail.com>
2023-08-21 17:02:34 -0300

    conceito teorema do limite central

---
+++ 04-Conceito_Teorema_do_Limite_Central.R
@@ -0,0 +1,71 @@
+# Conceito Teorema do Limite Central
+
+# Configurando o diretório de trabalho
+setwd("C:/Users/Julia/Desktop/CienciaDeDados/1.Big-Data-Analytics-com-R-e-Microsoft-Azure-Machine-Learning/10.Introducao-a-Analise-Estatistica-de-Dados---Parte-3")
+getwd()
+
+
+
+# O Teorema do Limite Central é um dos teoremas mais importantes da estatística, e é a essência da estatistica inferencial.
+
+
+
+# - O Teorema do Limite Central é um importante conceito da estatística e a demonstração de muitos outros teoremas estatísticos dependem dele, tanto
+#   que é conhecido como a "mãe de todos os teoremas" e realmente merece nossa atenção.
+
+
+# - Esse teorema afirma que quando o tamanho da amostra aumenta, a distribuição amostral da sua média aproxima-se cada vez mais de uma ditribuição
+#   normal.
+
+# - Esse teorema é fundamental na teoria da inferência estatística e sem este teorema, provavelmente a estatística não teria avançado a ciência
+#   que é hoje.
+
+
+# - A média das médias amostras será a média populacional.
+
+
+# - De acordo com o Teorema do Limite Central, médias amostrais de amostras suficientemente grandes, retiradas de qualquer população, terão uma
+#   distribuição normal.
+
+# - Se a população seguir uma distribuição normal de probabilidade, as médias amostrais também terão distribuição normal, independente do tamanho
+#   das amostras.
+
+
+
+
+# - O Teorema do Limite Central é um princípio estatístico que afirma que, à medida que o tamanho das amostras aumenta, a distribuição das médias 
+#   amostrais se aproxima de uma distribuição normal, independentemente da distribuição da população original. Isso é verdade mesmo que a população 
+#   original não seja normalmente distribuída. Em outras palavras, à medida que coletamos mais amostras e calculamos suas médias, a média das médias
+#   amostrais se aproximará da média da população e a variabilidade das médias amostrais diminuirá.
+
+## O Teorema do Limite Central é importante porque:
+  
+# -> Permite Generalização: Ele nos permite fazer inferências sobre a média populacional (parâmetro) com base nas médias amostrais (estatística),
+#    independentemente da distribuição original dos dados.
+
+# -> Justifica o Uso da Distribuição Normal: Mesmo que os dados originais não sigam uma distribuição normal, o teorema nos permite usar aproximações
+#    baseadas na distribuição normal para estimar probabilidades e intervalos de confiança.
+
+# -> Base da Estatística Inferencial: Muitos testes de hipóteses e intervalos de confiança dependem do Teorema do Limite Central para fornecer
+#    estimativas confiáveis de parâmetros populacionais.
+
+# -> Aplicações Práticas: O Teorema do Limite Central é amplamente aplicado em diversos campos, como economia, ciências sociais, biologia, medicina
+#    e engenharia, onde os pesquisadores frequentemente trabalham com amostras e precisam fazer inferências sobre populações maiores.
+
+
+# Em resumo, o Teorema do Limite Central é uma das pedras angulares da estatística inferencial, permitindo que façamos conclusões confiáveis sobre
+# populações com base em amostras, mesmo quando as distribuições originais não são normais.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
