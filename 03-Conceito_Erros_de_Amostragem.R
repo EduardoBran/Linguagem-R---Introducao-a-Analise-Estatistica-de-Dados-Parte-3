126fb2ae6f5eb41e3d140b1e13ea404dadc35583
Eduardo Brandao <eduardo.ads1814@gmail.com>
2023-08-21 16:48:40 -0300

    conceito erros de amostragem

---
+++ 03-Conceito_Erros_de_Amostragem.R
@@ -0,0 +1,35 @@
+# Conceito Erros de Amostragem
+
+# Configurando o diretório de trabalho
+setwd("C:/Users/Julia/Desktop/CienciaDeDados/1.Big-Data-Analytics-com-R-e-Microsoft-Azure-Machine-Learning/10.Introducao-a-Analise-Estatistica-de-Dados---Parte-3")
+getwd()
+
+
+
+# Revisando que são Parâmetros e Estatísticas
+
+# -> Parâmetros:   valores que descrevem características da população, como média e mediana da população.
+#                  ou seja, valores numéricos que descrevem características específicas da população como um todo.
+
+# -> Estatísticas: valores calculados a partir da amostra, como média e mediana da amostra.
+#                  ou seja, valores calculados a partir de uma amostra retirada da população.
+
+
+# - Como as Estatísticas são calculadas a partir da amostra, que é uma parte da população, não seria razoável esperar que a média dos dados da
+#   amostra fosse igual a média dos dados da população ?
+
+# - Seria então esperado que ambas as médias fossem iguais caso não houvessem erros.
+
+# - A diferença entre estes 2 valores é chamada de "erro de amostragem da média da amostra".
+
+# - E por que esses erros ocorreriam? 
+#   -> Porque a amostragem não foi feita de maneira adequada.
+#   -> Porque a amostra não é representativa da população.
+#   -> Porque não temos uma amostra com tamanho suficiente para fazer inferência sobre a população.
+#   -> Entro outros...
+
+# - Quanto maior o tamanho da amostra, menor será o erro da amostragem.
+
+
+
+
