c77e5750f9eac2039d45dde87f97aa7868c8e5b1
Eduardo Brandao <eduardo.ads1814@gmail.com>
2023-08-23 17:48:18 -0300

    escrevendo um resumo dos conceitos ate aqui

--- 07-Conceito_Nivel_de_Confianca.R
+++ 07-Conceito_Nivel_de_Confianca.R
@@ -36,6 +36,37 @@ library(dplyr)
 
 
 
+## Processo de inferência estatística e o objetivo final desse processo.
+
+# - Objetivo da Inferência Estatística: O objetivo fundamental da inferência estatística é tirar conclusões sobre uma população com base nas 
+#   informações contidas em uma amostra representativa dessa população. Isso é especialmente útil quando não é viável ou prático estudar a
+#   população inteira.
+
+# - Coleta de Amostras: Em vez de trabalhar com a população inteira, a inferência estatística envolve coletar amostras usando várias técnicas de 
+#   amostragem, como amostragem aleatória simples, estratificada, sistemática etc. A amostra deve ser representativa para garantir que as 
+#   conclusões sejam válidas para a população.
+
+# - Interpretação dos Resultados: Após coletar amostras e calcular estatísticas amostrais, como médias amostrais, desvios padrão amostrais etc.,
+#   o próximo passo é interpretar esses resultados. Isso envolve a construção de intervalos de confiança e a realização de testes de hipóteses.
+
+# - Inferência da Média da População: Com base na interpretação dos resultados da amostra, é possível fazer inferências sobre a média da população.
+#   Os intervalos de confiança fornecem uma faixa de valores dentro da qual acredita-se que a média populacional esteja, com uma determinada 
+#   probabilidade de confiança.
+
+# - Nível de Confiança: O nível de confiança é a probabilidade associada aos intervalos de confiança. Ele reflete quão certo estamos de que o
+#   parâmetro populacional está contido no intervalo calculado.
+
+# - Processo Iterativo: A inferência estatística é um processo iterativo. À medida que a coleta de dados e análises avançam, as conclusões podem 
+#   ser refinadas e ajustadas com base nas informações adicionais obtidas.
+
+
+# Em resumo, a inferência estatística nos permite obter informações valiosas sobre uma população com base em amostras representativas, ajudando-nos
+# a tomar decisões informadas e tirar conclusões confiáveis sobre o mundo ao nosso redor.
+
+
+
+
+
 # E qual a relação entre o Nível de Confiança e o Escore z?
 
 
---
+++ 08-Resumo.R
@@ -0,0 +1,78 @@
+# Resumo
+
+# Configurando o diretório de trabalho
+setwd("C:/Users/Julia/Desktop/CienciaDeDados/1.Big-Data-Analytics-com-R-e-Microsoft-Azure-Machine-Learning/10.Introducao-a-Analise-Estatistica-de-Dados---Parte-3")
+getwd()
+
+
+library(dplyr)
+
+
+
+
+# - A matemática em si é básica, não tem mistério. A questão é crompreender o problema e saber usar a ferramenta correta.
+
+# -> Vamos trabalhar com teorema do limite central
+# -> Aprender como calcular erro de amostragem
+# -> onde aplicar o escore z
+# -> como calcular e encontrar o nível de confiança
+# -> qual nível de significância ideal para que se possa chegar ao intervalo de confiança e partir daí encontrar ou prever onde está a 
+#    média da população.
+
+
+# Expandindo o resumo acima 
+
+
+# - Embora a matemática subjacente à estatística possa ser relativamente simples, a chave para uma análise estatística bem-sucedida reside na
+#   compreensão do problema em questão e na aplicação correta das ferramentas estatísticas disponíveis. Vou resumir os tópicos que você mencionou
+#   para reforçar o caminho que você está planejando seguir:
+  
+# -> Teorema do Limite Central:
+# -  Entender o Teorema do Limite Central é fundamental, pois ele é a base da estatística inferencial. Isso nos permite compreender como as médias
+#    amostrais se comportam e como podemos fazer inferências sobre a população.
+
+# -> Cálculo do Erro de Amostragem: 
+# -  Aprender a calcular o erro de amostragem é essencial para compreender a precisão das estimativas amostrais e como elas se relacionam com a
+#    população.
+
+# -> Escore z: 
+# -  Aprender como usar o escore z é importante para padronizar valores e entender quão distante estão da média populacional.
+
+# -> Cálculo e Interpretação do Nível de Confiança: 
+# -  Saber como calcular e interpretar intervalos de confiança com diferentes níveis de confiança é crucial para comunicar a incerteza em torno
+#    das estimativas.
+
+# -> Nível de Significância:
+# -  Compreender como o nível de significância está relacionado ao nível de confiança e como ele afeta testes de hipóteses e decisões estatísticas.
+
+# -> Aplicações Práticas:
+# -  Praticar a aplicação desses conceitos em problemas do mundo real é fundamental para fortalecer sua compreensão e habilidades estatísticas.
+
+# - Se você se concentrar nesses tópicos e se familiarizar com a aplicação prática deles, estará bem equipado para realizar análises estatísticas
+#   eficazes e tomar decisões informadas com base em dados. A prática e a exploração de exemplos reais são importantes para internalizar esses 
+#   conceitos e desenvolver confiança em sua capacidade de aplicá-los.
+
+
+
+## Respondendo Perguntas de Negócio com base nos conhecimentos até aqui
+
+# -> Tratar os dados
+# -> Coletar Amostras
+# -> Calcular o Intervalo de Confiança
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Vamos compreender agora o que é o valor crítico e como o escore z está diretamente relacionado ao nível de confiança.
+
+
