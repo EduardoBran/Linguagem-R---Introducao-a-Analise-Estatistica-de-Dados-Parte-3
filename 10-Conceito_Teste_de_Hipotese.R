6f3e2abddb2883dbcd2506c56505986874ee869d
Eduardo Brandao <eduardo.ads1814@gmail.com>
2023-08-26 17:38:17 -0300

    conceito sobre teste de hipotese

---
+++ 10-Conceito_Teste_de_Hipotese.R
@@ -0,0 +1,81 @@
+# Teste de Hipóteste (Definição)
+
+# Configurando o diretório de trabalho
+setwd("C:/Users/Julia/Desktop/CienciaDeDados/1.Big-Data-Analytics-com-R-e-Microsoft-Azure-Machine-Learning/10.Introducao-a-Analise-Estatistica-de-Dados---Parte-3")
+getwd()
+
+
+library(dplyr)
+
+
+# Definição do Teste de Hipótese
+
+# - No mundo da Estatística, uma hipóteste é uma suposição sobre um parâmetro específico de uma população, tal como média, proporção ou desvio padrão.
+
+# - Podemos fazer uma suposição sobre o valor de um parâmetro da população, coletar uma amostra desta população, medir a amostra e atestar se a
+#   amostra suporta ou não a nossa suposição.
+
+
+# - O mundo dos negócios é cheio de exemplos que lidam com testes de hipóteses:
+
+## Exemplo 1:
+
+#  -> Uma indústria de lâmpadas que fabrica lâmpadas fluorescentes afirma que suas lâmpadas usam 75% menos energia e duram 10 vezes mais que as 
+#     lâmpadas comuns.
+
+# - Neste caso, um laboratório independente poderia testar a afirmação do exemplo acima com um teste de hipótese.
+
+
+## Exemplo 2:
+
+#  -> Um artigo recente de um grande jornal, afirmou que o excesso de tempo em redes sociais poderia afetar a capacidade intelectual das pessoas.
+
+# - Neste caso, um pesquisar independente poderia validar esta afirmação usando um teste de hipótese.
+
+
+## Exemplo 3:
+
+#  -> Antes da crise, os bancos cobravam em média R$40 em taxas administrativas de contas correntes de pessias físicas.
+
+# - Neste caso, um orgão regulador do governo poderia testar a hipótese de os bancos estarem cobrando mais de R$40 em taxas.
+
+
+
+# - Teste de Hipótese é um dos procedimentos estatísticos mais usados atualmente, com muitas aplicações em Data Science, como em Testes A/B
+#   aplicados por equipes de Marketing por exemplo.
+
+
+# - O objetivo do Teste de Hipótese é decidir se determinada afirmação sobre um parâmetro populacional é ou não apoiada pela evidência obtida de
+#   dados amostrais.
+
+
+# - Cada teste de hipótese tem uma hipótese nula e uma hipótese alternativa, representados por:
+
+#  -> H0 - Hipóteste Nula         (afirmação inválida)
+#  -> Ha - Hipótese Alternativa.  (afirmação válida)
+
+# - Precisamos ser cuidadoso ao definir a hipótese nula e a hipótese alternativa.
+#   As decisões sobre as hipótestes dependem da natureza do teste e da pessoa que o está conduzindo.
+
+
+## Resumo:
+
+# - Os testes de hipótese são uma ferramenta crítica para a tomada de decisões informadas com base em evidências estatísticas. Eles permitem que
+#   as afirmações sobre parâmetros populacionais sejam avaliadas de forma sistemática e ajudam a mitigar decisões baseadas em observações casuais.
+
+# - A formulação adequada das hipóteses e a interpretação cuidadosa dos resultados são essenciais para a aplicação bem-sucedida dos testes de
+#   hipótese.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
